a0aa3f96f2a9cacf4d0a3d7e8a54397d476162eb
meghbhalerao <33202960+meghbhalerao@users.noreply.github.com>
2019-01-13 14:34:41 +0530

    Add files via upload
    Classification code for CIFAR in MATLAB, but it can be extended to classifying malignant and benign tumors by some modifications in the code

---
+++ CNN_GPU.m
@@ -0,0 +1,47 @@
+categories = {'Deer','Dog','Frog','Cat'};
+rootFolder = 'cifar10Train';
+imds = imageDatastore(fullfile(rootFolder, categories), ...
+    'LabelSource', 'foldernames');
+
+varSize = 32;
+conv1 = convolution2dLayer(5,varSize,'Padding',2,'BiasLearnRateFactor',2);
+conv1.Weights = gpuArray(single(randn([5 5 3 varSize])*0.0001));
+fc1 = fullyConnectedLayer(64,'BiasLearnRateFactor',2);
+fc1.Weights = gpuArray(single(randn([64 576])*0.1));
+fc2 = fullyConnectedLayer(4,'BiasLearnRateFactor',2);
+fc2.Weights = gpuArray(single(randn([4 64])*0.1));
+layers = [
+    imageInputLayer([varSize varSize 3]);
+    conv1;
+    maxPooling2dLayer(3,'Stride',2);
+    reluLayer();
+    convolution2dLayer(5,32,'Padding',2,'BiasLearnRateFactor',2);
+    reluLayer();
+    averagePooling2dLayer(3,'Stride',2);
+    convolution2dLayer(5,64,'Padding',2,'BiasLearnRateFactor',2);
+    reluLayer();
+    averagePooling2dLayer(3,'Stride',2);
+    fc1;
+    reluLayer();
+    fc2;
+    softmaxLayer()
+    classificationLayer()];
+
+
+opts = trainingOptions('sgdm', ...
+    'InitialLearnRate', 0.001, ...
+    'LearnRateSchedule', 'piecewise', ...
+    'LearnRateDropFactor', 0.1, ...
+    'LearnRateDropPeriod', 8, ...
+    'L2Regularization', 0.004, ...
+    'MaxEpochs', 10, ...
+    'MiniBatchSize', 100, ...
+    'Verbose', true);
+
+
+[net, info] = trainNetwork(imds, layers, opts);
+
+
+rootFolder = 'cifar10Test';
+imds_test = imageDatastore(fullfile(rootFolder, categories), ...
+    'LabelSource', 'foldernames');
